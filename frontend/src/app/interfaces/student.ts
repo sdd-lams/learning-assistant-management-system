fdc46b68628476c1f0b0a15ec81d57cf7127d64b
HuyNChu <chuhuyrpi@gmail.com>
2022-10-18 00:11:06 -0400

    update log in page css and html

--- frontend/src/app/interfaces/student.ts
+++ frontend/src/app/interfaces/student.ts
@@ -1,20 +1,22 @@
 export interface Student {
+  // Data that is parsed directly
   fname: String;
   lname: String;
-  dorm: String;
-  room: String;
-  status: String;
   rin: Number;
   email: String;
-  emailcount: Number;
-  ewscount: Number;
-  ewsreason: String;
+  dorm: String;
+  room: String;
   ewsdate: String;
+  ewsreason: String;
   cname: String;
   csubject: String;
   ccode: String;
   cprof?: String;
   profcomment?: String;
+  // Data that is computed or set by the LA
+  emailcount: Number; // Should init to 0
+  ewscount: Number; // Should be computed automatically when an entry is added to DB
+  status?: String;
   lacomment?: String;
   assignedla?: String;
 }
