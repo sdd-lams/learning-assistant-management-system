eb16577879f3e4569d9f0a56d12b31b63f2c0b61
Carter Ellis <carter.d.ellis10@gmail.com>
2022-10-23 17:04:17 -0400

    Finished student routes/ addManyStudents needs data parsing

--- backend/routes/student-routes.js
+++ backend/routes/student-routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Get all students
 router.get("/", async (req, res) => {
   try {
-    const data = await Student.find({ });
+    const data = await Student.find({  });
 
     console.log("Returned all the Students");
     res.status(200).json(data);
@@ -15,12 +15,14 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:rin/:ccode", async (req, res) => {
+router.get("/:rin", async (req, res) => {
   // Check to see if Student exists
   try {
     const exists = await Student.exists({
       rin: req.params.rin,
-      ccode: req.params.ccode
+      ccode: req.query.ccode,
+      csubject: req.query.csubject,
+      ewsdate: req.query.ewsdate
     });
 
     // If it exists, attempt to return it
@@ -28,9 +30,14 @@ router.get("/:rin/:ccode", async (req, res) => {
       try {
         const data = await Student.findOne({
           rin: req.params.rin,
-          ccode: req.params.ccode,
+          ccode: req.query.ccode,
+          csubject: req.query.csubject,
+          ewsdate: req.query.ewsdate,
         });
-        console.log("Returned the Student: " + data.fname + " " + data.lname);
+
+        console.log(`Returned the EWS entry created on ${data.ewsdate}\
+        for ${data.fname} ${data.lname} in course ${data.csubject}${data.ccode}`);
+
         res.status(200).json(data);
       } catch (error) {
         console.log("Could not get Student: " + error.message);
@@ -48,22 +55,66 @@ router.get("/:rin/:ccode", async (req, res) => {
   }
 });
 
-router.put("/:rin/:ccode", async (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const docs = await Student.insertMany(req.body);
+
+    console.log(`Inserted ${docs.length} new EWS entries`);
+    res.status(200).json({ message: `Successfully inserted ${docs.length} EWS entries` });
+  } catch (error) {
+    console.log("Error inserting new EWS data: " + error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put("/:rin", async (req, res) => {
   try {
     await Student.findOneAndUpdate(
       {
         rin: req.params.rin,
-        ccode: req.params.ccode,
+        ccode: req.query.ccode,
+        csubject: req.query.csubject,
+        ewsdate: req.query.ewsdate,
       },
       {
         status: req.body.status,
         cprof: req.body.cprof,
+        assignedla: req.body.assignedla,
       }
     );
-    console.log("Updated a goal for " + req.user.name);
-    res.status(200).json({ message: "Goal updated" });
+    console.log(`EWS entrty for rin:${req.params.rin}\
+    course:${req.query.csubject}${req.query.ccode} date:${req.query.ewsdate} was updated`);
+    res.status(200).json({ message: "EWS entry updated" });
   } catch (error) {
-    console.log("Error updating a goal: " + error.message);
+    console.log("Error updating EWS entry: " + error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    await Student.deleteMany({ });
+    console.log(`All EWS entries have been deleted`);
+    res.status(200).json({ message: "All EWS entries deleted" });
+  } catch (error) {
+    console.log("Error deleting EWS entries: " + error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:rin", async (req, res) => {
+  try {
+    await Student.deleteOne({
+      rin: req.params.rin,
+      ccode: req.query.ccode,
+      csubject: req.query.csubject,
+      ewsdate: req.query.ewsdate,
+    });
+    console.log(`EWS entrty for rin:${req.params.rin}\
+    course:${req.query.csubject}${req.query.ccode} date:${req.query.ewsdate} was deleted`);
+    res.status(200).json({ message: "EWS entry deleted" });
+  } catch (error) {
+    console.log("Error deleting EWS entry: " + error.message);
     res.status(500).json({ message: error.message });
   }
 });
