6c075ee5dd1a663243f556ec9c29e845ba1fccc5
Carter Ellis <carter.d.ellis10@gmail.com>
2022-11-09 10:41:24 -0500

    conflicts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { StudentListComponent } from './components/student-list/student-list.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { AuthGuard } from './guards/auth.guard';
@@ -13,7 +14,10 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [AuthGuard],
-    children: [{ path: '', redirectTo: '', pathMatch: 'full' }],
+    children: [
+      { path: '/students', component: StudentListComponent, pathMatch: 'full' },
+      //{ path: '/users', component: UserListComponent, pathMatch: 'full' },
+    ],
   },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
